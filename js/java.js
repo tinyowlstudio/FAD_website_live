37abbe063d481cecf896661d299fcf0c78bd674f
Alioran <92649375+Alioran@users.noreply.github.com>
2021-11-01 14:37:08 -0700

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -1,26 +1,3 @@
 function hideMenu() {
     document.getElementById("checkbox").checked = false;
 }
-
-//jQuery(document).ready(function(){
-//   
-//    jQuery('a[href^="#"]').click(function(e) {
-//
-//        if($(window).width() > 375) {
-//            jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
-//
-//            return false;
-//
-//            e.preventDefault();
-//        }
-//
-//    });
-//
-//});
-
-//function addMargin(){
-////    if($(window).width() > 375) {
-//        window.scrollTo(0, window.pageYOffset - 100);
-////    }
-//}
-//window.addEventListener('hashchange',addMargin);
