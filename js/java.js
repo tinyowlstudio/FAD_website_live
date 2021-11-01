42b41cd8d466a13b6ddfd8086171bc0a7746944f
Alioran <92649375+Alioran@users.noreply.github.com>
2021-11-01 15:49:06 -0700

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -1,3 +1,4 @@
+//This is for the mobile menu bar to close since the source code uses a checkbox for the hamburger icon
 function hideMenu() {
     document.getElementById("checkbox").checked = false;
 }
